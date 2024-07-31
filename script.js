9712b4d156172230fa58ccf85de51a03e1f09bac
Luis M <150566024+l-mina@users.noreply.github.com>
2024-07-31 17:03:51 -0400

    JS to check for matching password

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//sign-up form
+
+//Validate matching passwords
+const password = document.querySelector("#password");
+const confirm_password = document.querySelector("#confirm_password")
+const submit_button = document.querySelector("#submit");
+const password_text = document.querySelector("#password_text");
+const confirm_text = document.querySelector("#confirm_text");
+
+function PassCheck(){
+    if(password.value != confirm_password.value){
+        submit_button.disabled = true;
+        password_text.innerText="Password doesn't match.";
+    }
+    else {
+        submit_button.disabled = false;
+        password_text.innerText=" ";
+    }
+}
